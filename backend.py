0b769bc012398599b8e40cab34dd06218779abba
ZhouyaoXie <zhouyaox@andrew.cmu.edu>
2023-09-15 22:14:18 -0700

    convert utc time to pst time

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import os
 import requests
 from datetime import datetime, timedelta
 from base64 import b64encode
+import pytz 
 
 email, password = st.secrets["email"], st.secrets['password']
 
@@ -39,6 +40,26 @@ def get_time_entries(api_token, user_agent="api_example"):
     return entries 
 
 
+def utc_to_pst(utc_time_str, input_format="%Y-%m-%d %H:%M:%S"):
+    """
+    Convert UTC time string to PST time string.
+    
+    Args:
+    - utc_time_str (str): Input UTC time in string format.
+    - input_format (str, optional): Format of the input UTC time string. Defaults to "%Y-%m-%d %H:%M:%S".
+
+    Returns:
+    - str: PST time string in the same format as the input.
+    """
+    
+    utc_time = datetime.strptime(utc_time_str, input_format)
+    utc_time = pytz.utc.localize(utc_time)
+
+    pst_time = utc_time.astimezone(pytz.timezone('US/Pacific'))
+
+    return pst_time.strftime(input_format)
+
+
 def get_current_entry():
     url = 'https://api.track.toggl.com/api/v9/me/time_entries'
     auth_token = b64encode(f"{email}:{password}".encode()).decode("ascii")
@@ -55,10 +76,16 @@ def get_current_entry():
     try:
         for i in range(len(data_json)):
             if data_json[i]['project_id'] in project_id_mapping:
-                start_time = data_json[i]['start'].replace('+00:00', '').replace('T', ' ').replace('Z', '')
-                stop_time = data_json[i]['stop'].replace('+00:00', '').replace('T', ' ').replace('Z', '') if data_json[i]['stop'] is not None else 'now'
+                start_time_utc = data_json[i]['start'].replace('+00:00', '').replace('T', ' ').replace('Z', '')
+                start_time = utc_to_pst(start_time_utc)
+                stop_time_utc = data_json[i]['stop'].replace('+00:00', '').replace('T', ' ').replace('Z', '') if data_json[i]['stop'] is not None else 'now'
+                stop_time = utc_to_pst(stop_time_utc) if stop_time_utc != "now" else "now"
+
                 project = project_id_mapping[data_json[i]['project_id']]
-                description = ': ' + data_json[i]['description'] if data_json[i]['description'] is not None else ''
+                if data_json[i]['description'] is not None:
+                    description = ': ' + data_json[i]['description']
+                else:
+                    description = ''
                 msg = [
                     "from *{start}* to *{stop}*,".format(start = start_time, stop = stop_time), 
                     "zhouyao is spending her time on:",
